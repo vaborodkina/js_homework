accb5ad4c51fbf54c060467e92016bbfd4ae9701
Borodkina Valeriia <Valeriia.Borodkina@contracted.pmi.com>
2025-09-01 21:37:56 +0200

    Formatting fixed

--- HW_1.js
+++ HW_1.js
@@ -2,12 +2,10 @@
 
 null + 3; //3 -> 0+3=3
 
-5 &&
-  "qwerty" /*qwerty -> && returns 1st "false", as 5 is true and qwerty is true,
-              - it returns the last value*/ +
-    "40" +
-    +"2" +
-    "hillel"; //42hillel -> 40+2 and concatenation
+// prettier-ignore
+5 &&"qwerty" //qwerty -> && returns 1st "false", as 5 is true and qwerty is true,- it returns the last value 
+
++'40' + +'2' + "hillel"; //42hillel -> 40+2 and concatenation
 
 "10" - 5 === 6; //false -> 10-5=5; 5!=6
 
@@ -29,13 +27,10 @@ null == ""; //false -> ONLY null == undefined is true
 
 !!"false" == !!"true"; //true -> Not empty string = true; true == true
 
-0 ||
-  ("0" &&
-    1(
-      //1 -> && has the priority on the || -> 0 || ('0' && 1) -> 0 || 1
+// prettier-ignore
+0 || '0' && 1 //1 -> && has the priority on the || -> 0 || ('0' && 1) -> 0 || 1
 
-      +null == false
-    ) < 1); //false -> false == 0; true == 1 -> (0 == 0) <1 -> 1<1
+(+null == false) < 1; //false -> false == 0; true == 1 -> (0 == 0) <1 -> 1<1
 
 (false && true) || true; //true -> true || false -> true
 
