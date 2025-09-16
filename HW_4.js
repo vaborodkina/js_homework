90ef492216aebfb77845c149726d6ba35c45c5ff
Borodkina Valeriia <Valeriia.Borodkina@contracted.pmi.com>
2025-09-16 12:25:46 +0200

    Updated HW_4.js

--- HW_4.js
+++ HW_4.js
@@ -1,12 +1,12 @@
-const min = 100; //ліва границя включно
-const max = 1000; //права границя включно
+const min = 100; //ліва границя
+const max = 1000; //права границя
 
 function checkProbabilityTheory(count) {
   let counterOdd = 0; //лічильник для непарних чисел
   let counterEven = 0; //лічильник для парних чисел
   for (let i = 0; i < count; i++) {
     const randomNum = Math.floor(Math.random() * (max - min + 1)) + min; //+ min як зсув діапазону
-    console.log("Рандомне число: " + randomNum); // Виведе рандомне число число від 100 до 1000
+    console.log("Рандомне число: " + randomNum); // Виведе рандомне число число від 100 до 1000 включно
     if (randomNum % 2 === 0) {
       counterEven++;
     } else {
